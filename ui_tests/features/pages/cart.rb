b0c6ffafe72c50f50aea9adcd93f5e306ea15abe
Beatriz <beatriz.lana@stoodi.com.br>
2021-01-07 21:53:54 -0300

    Fix element not found

--- ui_tests/features/pages/cart.rb
+++ ui_tests/features/pages/cart.rb
@@ -1,10 +1,12 @@
 class Cart < SitePrism::Page
     set_url ''
 
-    element :go_to_cart, 'a[@href="http://automationpractice.com/index.php?controller=order"]'
-    element :continue_shopping_btn, '.clearfix .continue .btn .btn-default .button .exclusive-medium'
+    element :go_to_cart, :xpath, 'a[@href="http://automationpractice.com/index.php?controller=order"]'
+    element :continue_shopping_btn, '.continue .btn .btn-default .button .exclusive-medium'
 
     def continue_shopping
-        continue_shopping_btn.click
+        if continue_shopping_btn.is_visible?
+            continue_shopping_btn.click
+        end
     end
 end
